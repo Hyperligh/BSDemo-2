7134d8e59fe2d2381eb38b4ea736097b406f8a8a
Hyperligh <hyperligh19@hotmail.com>
2016-02-07 15:36:33 -0800

    First prototype of layout

--- index.php
+++ index.php
@@ -6,12 +6,16 @@
 	<meta charset="utf-8">
 	<title>Bootstrap Demo</title>
 	<link rel="stylesheet" href="style/bootstrap/css/bootstrap.min.css">
+
 	<link rel="stylesheet" href="style/mystyle.css">
 	<link href='https://fonts.googleapis.com/css?family=Raleway:400,700|Droid+Sans' rel='stylesheet' type='text/css'>
 	<script src="jquery-1.8.3.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 	<script src="http://cdn.tinymce.com/4/tinymce.min.js"></script>
+	<!--div.editable-->
 	<script>	tinymce.init({
-				selector: 'div.editable',
+				selector: '',
 				inline: true,
 				plugins: [
 				    'advlist autolink lists link image charmap print preview anchor',
@@ -24,49 +28,64 @@
 </head>
 
 <body>
-<div class="container">
-
+	<div class="hero cf">
+		<img class="img-responsive" src="photos/hero.jpg" alt="">
 	
-	<header class="jumbotron">
+		<div class="tagLine bot"><p></p></div>
+		<div class="tagLine right1"><p></p></div>
+		<div class="tagLine right2"><p></p></div>
+	</div>
 
-		<!--Top navigation -->
-				<div id="small-nav-container"><div id="small-nav"></div>
-				<div id="small-nav"></div>
-				<div id="small-nav"></div>
-				<!-- <p class="close-close">X Close</p>-->	
-		<nav class="">
-			<ul class="close-close">
-				<li><a href="/">Home</a></li>
-				<li><a href="/">Photo Gallery</a></li>
-				<li><a href="/">About Us</a></li>
-				<li><a href="/">Directions</a></li>
-				<li><a href="/">Contact Us</a></li>
-			</ul>
-		</nav>
-				</div>
-		<!--End of Top Nav-->
-		<div class="contact-info"><p>000-647-0000</p><hr /></div>
-		<div class="contact-info"><p>email@email.com</p><hr /></div>
-		<div id class="logo"><a href="/">
+	<div id class="logo cf"><a href="/">
 		<img class="img-responsive" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=300%C3%97175&w=300&h=175"></a>
-		</div>
-</div>
+	</div>
 		
-		<div class="hero cf">
-			<img class="img-responsive" src="photos/hero-3.jpg" alt="">
-		</div>
+
+
+
+
+
+<nav role="navigation" class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        	<div class="navbar-header">
+		            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+		                <span class="sr-only">Toggle navigation</span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		            </button>
+            
+        	</div>
+        <!-- Collection of nav links and other content for toggling -->
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Themes</a></li>
+                <li class="dropdown"><a data-toggle="dropdown" class="dropdown-toggle" href="#">Parts<b class="caret"></b></a>
+                	<ul role="menu" class="dropdown-menu">
+                		<li><a href="#">Navs</li></a>
+                		<li><a href="#">Photo Galleries</li></a>
+                		<li><a href="#">Directions</li></a>
+                	</ul></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+		
+		
 		<div class="cta">
-				<h3><a href="#">Contact Me</a><a href="themes.html">Visit Themes</a></h3>
+				<h3><a href="forms.php">Contact Me</a><a href="themes.html">Visit Themes</a></h3>
 			</div>	
 </header>
 <div class="container-fluid">			
 	
-		<div class="row content">
+		<div class="row odd content">
 			<div class="col-lg-12 editable">
 			<h1>Bootstrap  <strong>Classes</strong></h1>
 			</div>
 		</div>
-			<div class="row content">
+			<div class="row odd content">
 					<div class="col-md-4 editable">
 					
 					</div>
@@ -117,6 +136,32 @@
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad  </p>
 					</div>
 				</div>	
+
+
+				<div class="row content odd ">
+					<div class="col-md-6">
+						<p>Hello</p>
+						<img class="img-responsive" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=325×225&w=825&h=425">
+					</div>
+
+					<div class="col-md-6">
+						<p>Hello</p>
+						<p>Hello</p>
+						<p>Hello</p>
+						<p>Hello</p>
+						<p>Hello</p>
+						<p>Hello</p>
+						<p>Hello</p>
+						<p>Hello</p>
+						<p>Hello</p>
+						<p>Hello</p>
+						<p>Hello</p>
+
+					</div>
+				</div>	
+
+
+
 		<footer>
 		<div class="row">
 			<div class="col-lg-12">
@@ -124,11 +169,11 @@
 
 			</div>
 		</div>
-				<script>
+				<!--<script>
 					$('#small-nav-container').click(function(){
 						$('nav ul, #small-nav-container p').toggleClass('close-close');
 					});
-				</script>
+				</script>-->
 		</footer>
 		
 		
